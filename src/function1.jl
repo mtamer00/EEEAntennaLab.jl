fee3d4b9dbac01f2a16bb89654864f3236e7c601
MertTamer <merttamerkisisel@gmail.com>
2023-11-16 16:41:31 +0300

    function

--- src/function1.jl
+++ src/function1.jl
@@ -1,12 +1,6 @@
-module function1
- 
- export func1
+  export func1
  
  function func1(number::Float64)
      x=(number)^2;
      return x;
  end
-
-
-
-end
--- src/function2.jl
+++ src/function2.jl
@@ -1,11 +1,6 @@
-module function2
-  
  export func2
 
  function func2(number::Float64)
      x=(number)^3;
      return x;
  end
-
-
-end
